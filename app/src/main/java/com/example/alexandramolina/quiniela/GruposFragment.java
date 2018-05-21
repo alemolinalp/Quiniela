c8d876de31f1cabe47b32e80e69fd9b3d232309b
Andres Sancho <andrem.san12@gmail.com>
2018-05-21 16:17:07 -0600

    Grupos inicio

--- app/src/main/java/com/example/alexandramolina/quiniela/GruposFragment.java
+++ app/src/main/java/com/example/alexandramolina/quiniela/GruposFragment.java
@@ -3,7 +3,6 @@ package com.example.alexandramolina.quiniela;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class InicioFragment extends Fragment {
+public class GruposFragment extends Fragment {
 
 
-    public InicioFragment() {
+    public GruposFragment() {
         // Required empty public constructor
     }
 
@@ -24,13 +23,7 @@ public class InicioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
-
-
-
-
-        return view;
+        return inflater.inflate(R.layout.fragment_grupos, container, false);
     }
 
 }
